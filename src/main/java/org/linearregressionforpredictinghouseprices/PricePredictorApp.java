0e905ff6c836efc1540d5010732b7491272d2373
Septic-H <godriednuts@gmail.com>
2024-05-21 21:05:38 +0500

    Update

--- src/main/java/org/linearregressionforpredictinghouseprices/PricePredictorApp.java
+++ src/main/java/org/linearregressionforpredictinghouseprices/PricePredictorApp.java
@@ -1,4 +1,28 @@
 package org.linearregressionforpredictinghouseprices;
 
-public class PricePredictorApp {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class PricePredictorApp extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("UserInterface.fxml"));
+            BorderPane root = loader.load();
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("lightTheme.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Linear Regression for Predicting House Prices");
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/main/java/org/linearregressionforpredictinghouseprices/UserInterfaceController.java
+++ src/main/java/org/linearregressionforpredictinghouseprices/UserInterfaceController.java
@@ -1,4 +1,24 @@
 package org.linearregressionforpredictinghouseprices;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class UserInterfaceController {
+    @FXML
+    private void handlePredictPrice() {
+        // Implementation for price prediction
+    }
+
+    @FXML
+    private void handleDataVisualization() {
+        // Implementation for data visualization
+    }
+
+    @FXML
+    private void handleExit(ActionEvent event) {
+        Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
 }
